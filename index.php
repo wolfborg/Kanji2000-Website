d6cc22f58adb0b84ce8c77e7268aaaae49e5ad12
Derek Chaplin <wolfborg@users.noreply.github.com>
2017-07-13 10:30:23 -0400

    Converted HTML to PHP, Bootstrapped forms, and added style.css

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="css/style.css" media="screen">
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </head>
-
   <body>  
      <header>
       <div class="container">
         <nav class="navbar navbar-default">
           <ul class="nav navbar-nav">
-            <li class="nav-item active"><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Login</a></li>
+            <li class="nav-item active"><a href="/">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
           </ul>
         </nav>
       </div>
@@ -24,8 +24,10 @@
 
     <main>
       <div class="container">
-        <div class="row">
-          Hello World
+        <div class="row about">
+            <br/><br/><br/>
+            <p>Welcome to Kanji2000, a website that helps you learn and review the 2000 essential kanji characters needed to read and write basic Japanese.</p><br/>
+            <p>Through our quiz system, kanji database, and learning algorithm,<br/>you should be literate in Japanese kanji in no time at all!</p><br/><br/>
         </div>
       </div>
     </main>
