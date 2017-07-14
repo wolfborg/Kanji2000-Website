7459dab947dcfaf44a751f021f921a22a8cf30e9
wolfborg <wolfborg@users.noreply.github.com>
2017-07-14 09:57:25 -0400

    Clean up and modals

--- register.php
+++ register.php
@@ -29,68 +29,19 @@
           <form method="get" action="" id="mainForm">
              <fieldset>
                 <legend>Register an Account</legend>
-                <table>
-                   <tr>
-          			<td>
-                         <p>
-                         <label>First Name</label><br/>
-                            <input type="text" name="fName" size="25" class="required"/>
-                         </p>
-            			</td>
-          			<td>
-          			   <p>
-                         <label>Last Name</label><br/>
-                            <input type="text" name="title" size="25" class="required" />
-                         </p>
-          			 </td>
-                   </tr>
-          		 
-          		 <tr>
-                      <td colspan="2" >
-          			<label>Username</label><br/>               
-          				<input type="text" name="userName" size="25" class="required"/>
-                      </td>
-                   </tr>
-          		 
-          		 <tr>
-                      <td>
-                        <label>Email Address</label><br/>               
-          				<input type="text" name="userEmail" size="25" class="required"/>   
-                      </td>
-                   </tr>
-          		 
-                   <tr>
-                      <td> 
-                         <p>	
-                         <label>Enter Password</label><br/>               
-                         <input type="text" name="passWord" size="25" class="required"/>
-                         </p>
-                      </td>
-                      <td>
-                         <p>	
-                         <label>Confirm Password</label><br/>               
-          				<input type="text" name="passWord" size="25" class="required"/>
-                         </p>          
-                      </td>
-                   </tr>
-          		 
-          		 <tr>
-          			<td>
-          				<label>Enter You Skill Level</label><br/>
-          				<input type="radio" name="skill" value="beginner" class="required"> Beginner
-          				<input type="radio" name="skill" value="intermediate" class="required"> Intermediate
-          				<input type="radio" name="skill" value="expert" class="required"> Expert
-          			</td>
-          		 </tr>
-          		 
-                   <tr>
-                      <td colspan="2">
-                         <div class="rectangle centered"> 
-                            <input type="submit" class="rounded"> <input type="reset" value="Clear Form" class="rounded">  
-                         </div
-                      </td>
-                   </tr>
-                </table>
+                <label>First Name</label><br/><input type="text" name="fName" size="25" class="required"/>
+          			<label>Last Name</label><br/><input type="text" name="title" size="25" class="required" />
+          			<label>Username</label><br/><input type="text" name="userName" size="25" class="required"/>
+                <label>Email Address</label><br/><input type="text" name="userEmail" size="25" class="required"/>
+                <label>Enter Password</label><br/><input type="text" name="passWord" size="25" class="required"/>
+                <label>Confirm Password</label><br/><input type="text" name="passWord" size="25" class="required"/>
+          			<label>Enter You Skill Level</label><br/>
+                  <input type="radio" name="skill" value="beginner" class="required"/> Beginner
+          				<input type="radio" name="skill" value="intermediate" class="required"/> Intermediate
+          				<input type="radio" name="skill" value="expert" class="required"/> Expert
+                <div class="rectangle centered"> 
+                  <input type="submit" class="rounded"/><input type="reset" value="Clear Form" class="rounded"/>  
+                </div>
              </fieldset>
           </form>
         </div>
