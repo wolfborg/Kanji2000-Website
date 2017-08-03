ae9b267e43f2fbbbf89be9bd943eceea147f5bec
John <bowesj@wit.edu>
2017-08-03 19:07:38 -0400

    a

--- KanjiQuiz.js
+++ KanjiQuiz.js
@@ -155,7 +155,7 @@ function nextQuestion(e){
 
 
 	alert("New Question Answer" + answer);
-	setOptions
+	setOptions();
 		kanji =  document.getElementById("kanji").innerHTML;
 	answer = document.getElementById("english").innerHTML;
 
