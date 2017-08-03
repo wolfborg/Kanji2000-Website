ae8ab55ace9be9db9a47e9961941b5cf57c46af4
Derek Chaplin <wolfborg@users.noreply.github.com>
2017-08-03 15:46:36 -0400

    Register empty confirms fix

--- registration_validation.js
+++ registration_validation.js
@@ -18,7 +18,7 @@ window.onload = function(){
 			}
 		}
 
-		if (mainForm["email"].value != mainForm["confirmEmail"].value) {
+		if (mainForm["email"].value != null || mainForm["email"].value != mainForm["confirmEmail"].value) {
 			mainForm["email"].style.borderColor = "red";
 			mainForm["confirmEmail"].style.borderColor = "red";
 			e.preventDefault();
@@ -26,13 +26,12 @@ window.onload = function(){
 			if (mainForm["email"].style.borderColor == "red") {
 				mainForm["email"].style.borderColor = initialBorder;
 			}
-			
 			if (mainForm["confirmEmail"].style.borderColor == "red") {
 				mainForm["confirmEmail"].style.borderColor = initialBorder;
 			}
 		}
 
-		if (mainForm["password"].value != mainForm["confirmPassword"].value) {
+		if (mainForm["password"].value != null || mainForm["password"].value != mainForm["confirmPassword"].value) {
 			mainForm["password"].style.borderColor = "red";
 			mainForm["confirmPassword"].style.borderColor = "red";
 			e.preventDefault();
@@ -40,7 +39,6 @@ window.onload = function(){
 			if (mainForm["password"].style.borderColor == "red") {
 				mainForm["password"].style.borderColor = initialBorder;
 			}
-			
 			if (mainForm["confirmPassword"].style.borderColor == "red") {
 				mainForm["confirmPassword"].style.borderColor = initialBorder;
 			}
