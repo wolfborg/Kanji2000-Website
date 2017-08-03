98211c9a7912a4288eea2ad055b9727438626c8c
John <bowesj@wit.edu>
2017-08-03 18:20:25 -0400

    a

--- KanjiQuiz.js
+++ KanjiQuiz.js
@@ -156,9 +156,9 @@ function nextQuestion(e){
 	
 
 	setOptions();
-
 	$("#el_kanji").load(location.href + " #el_kanji");
-	
+	answer = document.getElementById("english").innerHTML;
+
 	//location.reload();
 
 
