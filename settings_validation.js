8fef62541e904a1dcd9041dc73d9ee18abbff0c0
Kieran Guinan <guinank1@wit.edu>
2017-08-03 19:40:43 -0400

    Settings functionality and clear forms

--- settings_validation.js
+++ settings_validation.js
@@ -1,48 +1,39 @@
 window.onload = function(){
 	var mainForm = document.getElementById("settings-form");
-
 	var initialBorder = mainForm["password"].style.borderColor;
 	//var reqs = document.getElementsByClassName("required");
 	mainForm.addEventListener("submit",function(e){
-		e.preventDefault();
-
-
-		/*
-		if (mainForm["password"].value == "" || mainForm["confirmPassword"].value == "") {
-			if(mainForm["password"].value != "" || mainForm["confirmPassword"].value != "" && mainForm["password"].value != mainForm["confirmPassword"].value){
-			mainForm["password"].style.borderColor = "red";
-			mainForm["confirmPassword"].style.borderColor = "red";
-			e.preventDefault();
-			}
-		} else {
-			if (mainForm["password"].style.borderColor == "red") {
-				mainForm["password"].style.borderColor = initialBorder;
-			}
-			if (mainForm["confirmPassword"].style.borderColor == "red") {
-				mainForm["confirmPassword"].style.borderColor = initialBorder;
-			}
-		}*/
 	
-		if(mainForm["password"].value == "" && mainForm["confirmPassword"].value == "" /*and if skill is blank*/){
+		if(mainForm["password"].value == "" && mainForm["confirmPassword"].value == "" && mainForm["skill"].value == ""){
 			mainForm["password"].style.borderColor = initialBorder;
 			mainForm["confirmPassword"].style.borderColor = initialBorder;
 			e.preventDefault();
 		}
-		else if(mainForm["password"].value != "" || mainForm["confirmPassword"].value != "" /*or if skill is not blank*/){
+		else{
+		 if(mainForm["password"].value != "" || mainForm["confirmPassword"].value != ""){
 			if(mainForm["password"].value !=""){
 				if(mainForm["password"].value != mainForm["confirmPassword"].value){
 					mainForm["password"].style.borderColor = "red";
 					mainForm["confirmPassword"].style.borderColor = "red";
 					e.preventDefault();
 				}
-			}else if(mainForm["confirmPassword"].value!=""){
+			}if(mainForm["confirmPassword"].value!=""){
 					mainForm["password"].style.borderColor = "red";
 					mainForm["confirmPassword"].style.borderColor = "red";
 					e.preventDefault();
 			}
-
-
 		}
+	}
 	
+	});
+
+	var closeButton = document.getElementById("settings-close");
+	closeButton.addEventListener("click", function(f){
+		mainForm["password"].value = "";
+		mainForm["confirmPassword"].value="";
+		mainForm["skill"]="";
+		mainForm["password"].style.borderColor = initialBorder;
+		mainForm["confirmPassword"].style.borderColor = initialBorder;
+
 	});
 }
