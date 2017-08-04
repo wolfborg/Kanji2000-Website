ed099089f98a72815fe205d64b1900367ea5a3cb
Derek Chaplin <wolfborg@users.noreply.github.com>
2017-08-04 09:04:03 -0400

    index button fix

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             <span class="icon-bar"></span><span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Kanji-2000</a></div>
+          <a class="navbar-brand" href="index.php">Kanji 2000</a></div>
 
           <div class="collapse navbar-collapse" id="topFixedNavbar1"> 
             <form class="navbar-form navbar-left" role="search">
