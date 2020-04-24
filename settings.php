918431e4fe01c691ccd624975e5f933df8b78aaf
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-23 21:13:08 -0400

    Fixed changing password and added changes for names

--- settings.php
+++ settings.php
@@ -13,16 +13,23 @@ function change_settings() {
 			echo $error;
 		}
 
-		// Checks if password is entered
+		// Checks for password change
 		if(isset($_POST["password"]) && !empty($_POST["password"]) && isset($_POST["confirmPassword"]) && !empty($_POST["confirmPassword"])) {
-			$algorithm = "sha256";
-			// Creates randomized salt for the new user
-			$salt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
-			$iterations = 10000;
-			// Creates PBKDF2 hashed password for the new user
-			$password = hash_pbkdf2($algorithm, $_POST["password"], $salt, $iterations);
+			$hash = password_hash($_POST["password"], PASSWORD_BCRYPT);
 		}
-		if(isset($_POST["skill"]) && !empty($_POST["skill"])){
+
+		// Checks for first name change
+		if(isset($_POST["firstName"]) && $_POST["firstName"] != $_SESSION["user_first_name"]) {
+			$firstName = $_POST["firstName"];
+		}
+
+		// Checks for last name change
+		if(isset($_POST["lastName"]) && $_POST["lastName"] != $_SESSION["user_last_name"]) {
+			$lastName = $_POST["lastName"];
+		}
+
+		// Checks for skill change
+		if(isset($_POST["skill"]) && !empty($_POST["skill"]) && $_POST["skill"] != $_SESSION["user_level"]){
 			switch($_POST["skill"]){
 				case "beginner":
 					$skill = 0;
@@ -35,10 +42,11 @@ function change_settings() {
 					break;
 			}
 		}
+
 		// Updates Password
-		if(isset($password)) {
+		if(isset($hash)) {
 			// Stores the new user's name, email, hashed password, and salt into the database
-			$sql = "UPDATE `users` SET `user_password` = " . db_quote($password) . ", `user_salt` = " . db_quote($salt) ." WHERE `user_id` = ".db_quote($_SESSION["user_id"]);
+			$sql = "UPDATE `users` SET `user_hash` = " . db_quote($hash) . " WHERE `user_id` = " . db_quote($_SESSION["user_id"]);
 			$result = db_query($sql);
 				
 			// Checks for result
@@ -48,9 +56,43 @@ function change_settings() {
 			}
 			else {
 				header("location: dashboard.php");
-				}
 			}
-		if(isset($skill)){
+		}
+
+		// Updates first name
+		if (isset($firstName)) {
+			$sql = "UPDATE `users` SET `user_first_name` = " . db_quote($firstName) ." WHERE  `user_id` = " . db_quote($_SESSION["user_id"]);
+			$result = db_query($sql);
+			
+			// Checks for result
+			if($result === false) {
+			    $error = db_error();
+			    echo $error;
+			}
+			else {
+				$_SESSION['user_first_name'] = $firstName;
+				header("location: dashboard.php");
+			}
+		}
+
+		// Updates last name
+		if (isset($lastName)) {
+			$sql = "UPDATE `users` SET `user_last_name` = " . db_quote($lastName) ." WHERE  `user_id` = " . db_quote($_SESSION["user_id"]);
+			$result = db_query($sql);
+			
+			// Checks for result
+			if($result === false) {
+			    $error = db_error();
+			    echo $error;
+			}
+			else {
+				$_SESSION['user_last_name'] = $lastName;
+				header("location: dashboard.php");
+			}
+		}
+
+		// Updates skill
+		if(isset($skill)) {
 			$sql = "UPDATE `users` SET `user_level` = " . db_quote($skill) ." WHERE  `user_id` = " . db_quote($_SESSION["user_id"]);
 			$result = db_query($sql);
 				
@@ -60,12 +102,12 @@ function change_settings() {
 			    echo $error;
 			}
 			else {
+				$_SESSION['user_level'] = $skill;
 				header("location: dashboard.php");
-				}
 			}
 		}
-				
-	}
+	}		
+}
 
 change_settings();
 
